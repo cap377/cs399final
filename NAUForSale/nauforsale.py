082cc7271c4964f4c5cd293eb29505b04b5d7330
alotaibimo <amm688@nau.edu>
2015-04-26 19:23:21 -0700

    added signup and login views and request handlers

--- NAUForSale/nauforsale.py
+++ NAUForSale/nauforsale.py
@@ -16,6 +16,7 @@
 #
 import os
 import urllib
+import logging
 
 from google.appengine.api import users
 from google.appengine.ext import ndb
@@ -43,15 +44,54 @@ class SplashHandler(webapp2.RequestHandler):
     def get(self):
         # this response(SPLASH_HTML) will load the html directly
         #self.response.write(SPLASH_HTML)
-        template_values = {}
+        logged_in = False
+        user = users.get_current_user()
+        logging.info("user: %s", user)
+
+        if user: 
+            logged_in = True
+        template_values = {
+            "user": {"nickname": user.nickname()}, 
+            "logged_in": logged_in,
+        }
         template = JINJA_ENVIRONMENT.get_template('test.html')
         self.response.write(template.render(template_values))
 
 class LoginHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Login Page Here!')
+        logged_in = False
+        user = users.get_current_user()
+        logging.info("user: %s", user)
+
+        if user: 
+            logged_in = True
+        template_values = {
+            "user": {"nickname": user.nickname()}, 
+            "logged_in": logged_in,
+        }
+        template = JINJA_ENVIRONMENT.get_template('login.html')
+        self.response.write(template.render(template_values))
+
+class SignupHandler(webapp2.RequestHandler):
+    def get(self):
+        logged_in = False
+        user = users.get_current_user()
+        logging.info("user: %s", user)
+
+        if user: 
+            logged_in = True
+        template_values = {
+            "user": {"nickname": user.nickname()}, 
+            "logged_in": logged_in,
+        }
+        template = JINJA_ENVIRONMENT.get_template('signup.html')
+        self.response.write(template.render(template_values))
+
+    def post(self):
+        self.response.write('Signin post Page Here!')
 
 app = webapp2.WSGIApplication([
     ('/', SplashHandler),
-    ('/login', LoginHandler)
+    ('/login', LoginHandler),
+    ('/signup', SignupHandler),
 ], debug=True)
