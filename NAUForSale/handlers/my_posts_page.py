9e5034a6146283f2ad68d245b55a4de87df611d8
Alex <ajs476@nau.edu>
2015-04-29 18:43:41 -0700

    myPosts functionality added

--- NAUForSale/handlers/my_posts_page.py
+++ NAUForSale/handlers/my_posts_page.py
@@ -1,6 +1,7 @@
 import webapp2
 from google.appengine.ext import ndb
 from handlers import BaseHandler
+from models import Post
 from google.appengine.api import users
 
 
@@ -8,8 +9,12 @@ class MyPostsPage(BaseHandler):
 	def get(self):
 
 		user = users.get_current_user()
+		q = Post.query().fetch(100)
 
 		if user:
-			self.render("myPosts.html", {})
+			self.render("myPosts.html", {
+                                "posts": q,
+                                "user": user
+                                })
 		else:
 			self.redirect(users.create_login_url(self.request.url))
