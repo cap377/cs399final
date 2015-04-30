d9cbc0689fe4fc395fc20e2ce11e366cc3d20b10
cap377 <cap377@nau.edu>
2015-04-29 18:52:59 -0700

    change to post model

--- NAUForSale/models/Post.py
+++ NAUForSale/models/Post.py
@@ -6,8 +6,8 @@ class Post(ndb.Model):
     post_date = ndb.DateProperty(auto_now=True)
     description = ndb.TextProperty(required=True)
     price = ndb.StringProperty(required=True)
-    picture = ndb.BlobProperty()
+    picture = ndb.BlobProperty(default="")
     contact_email = ndb.StringProperty(required=True)
-    contact_phone = ndb.StringProperty()
+    contact_phone = ndb.StringProperty(default="")
     category = ndb.StringProperty(choices=('Home, Garden & Tools', 'Beauty', 'Electronics & Computers', 'Clothing, Shoes & Jewelry',
                                            'Books', 'Movies, Music & Games', 'Sports & Outdoors', 'Automotive', 'Pets', 'Free'), required=True)
